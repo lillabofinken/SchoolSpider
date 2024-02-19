bdfe8f8af35fb27562dedbc86cf39b7533378bdf
Philip Bäcklund <philback@playgroundsquad.com>
2024-02-19 16:49:20 +0100

    Commit arson

---
+++ Source/SchoolSpider/SchoolSpider.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SchoolSpider.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SchoolSpider, "SchoolSpider" );
